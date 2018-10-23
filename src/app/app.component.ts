d7d4ad0bbd741818873f5a73efccc1fe4422b612
Victoria Oh <ohvictori@gmail.com>
2018-10-23 09:30:36 -0700

    Complete Epicodus Angular lessons through 'Using Variables in the Class Declaration.'

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
+  selector: 'to-do',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'app';
+
+  currentTime = new Date();
+  month: number = this.currentTime.getMonth() + 1;
+  day: number = this.currentTime.getDate();
+  year: number = this.currentTime.getFullYear();
+
+  currentFocus: string = 'Angular Homework';
+
+  firstTask = {
+    description: "Finish weekend Angular homework for Epicodus course."
+  }
 }
